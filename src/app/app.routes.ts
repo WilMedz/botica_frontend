fa4a7686fc9ca93de0fc92767731e00cbcfd6cce
Carlos Cueva <wolfgand28@gmail.com>
2026-05-17 17:12:51 -0500

    Creacion del CRUD con angular para proveedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ProveedorComponent } from './pages/proveedor/proveedor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'proveedores', component: ProveedorComponent },
+    { path: '', redirectTo: 'proveedores', pathMatch: 'full' }
+];
---
+++ src/app/model/proveedor.ts
@@ -0,0 +1,9 @@
+export interface Proveedor {
+    idProveedor?: number;
+    razonSocial: string;
+    ruc: string;
+    direccion: string;
+    telefono: string;
+    email: string;
+    estado: boolean;
+}
