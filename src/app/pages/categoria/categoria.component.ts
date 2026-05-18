bc9bc574757eb057801278f6c6d1f76db3d7de65
Santiago Lumba <santiago321lumbarojas456@gmail.com>
2026-05-18 00:06:35 -0500

    Actualizacion del estilo de categorias

--- src/app/pages/categoria/categoria.component.ts
+++ src/app/pages/categoria/categoria.component.ts
@@ -1,22 +1,78 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Categoria } from '../../model/categoria';
 import { CategoriaService } from '../../services/categoria.service';
 
 @Component({
   selector: 'app-categoria',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.css'
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit {
   private categoriaService = inject(CategoriaService);
+  private fb = inject(FormBuilder);
 
   categorias = signal<Categoria[]>([]);
+  categoriaForm: FormGroup;
+  isEditing = false;
+  editingId: number = 0;
 
   constructor() {
-    this.categoriaService.findAll().subscribe(data => {
-      this.categorias.set(data);
+    this.categoriaForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      descripcion: ['', [Validators.required]],
+      estado: [true]
     });
+  }
+
+  ngOnInit(): void {
+    this.cargarCategorias();
+  }
+
+  cargarCategorias(): void {
+    this.categoriaService.findAll().subscribe(data => this.categorias.set(data));
+  }
+
+  guardar(): void {
+    if (this.categoriaForm.invalid) {
+      alert('Corrige los errores del formulario');
+      return;
+    }
+    const categoria: Categoria = this.categoriaForm.value;
+    if (this.isEditing) {
+      this.categoriaService.update(categoria, this.editingId).subscribe(() => {
+        this.cargarCategorias();
+        this.resetForm();
+      });
+    } else {
+      this.categoriaService.save(categoria).subscribe(() => {
+        this.cargarCategorias();
+        this.resetForm();
+      });
+    }
+  }
+
+  editar(cat: Categoria): void {
+    this.isEditing = true;
+    this.editingId = cat.idCategoria;
+    this.categoriaForm.patchValue({
+      nombre: cat.nombre,
+      descripcion: cat.descripcion,
+      estado: cat.estado
+    });
+  }
+
+  eliminar(id: number): void {
+    if (confirm('¿Eliminar categoría?')) {
+      this.categoriaService.delete(id).subscribe(() => this.cargarCategorias());
+    }
+  }
+
+  resetForm(): void {
+    this.categoriaForm.reset({ estado: true });
+    this.isEditing = false;
+    this.editingId = 0;
   }
 }
